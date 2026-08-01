1e0f7a1caf31f9f444765cab545a9b7c7db1c841
usama228 <57168278+usama228@users.noreply.github.com>
2026-08-01 08:12:27 +0000

    Deploying to gh-pages from @ zainulhassan815/formz-kotlin@431e52690b5d78f44fe30570cd37bc10071a1d15 🚀

--- build.gradle.kts
+++
@@ -1,6 +0,0 @@
-plugins {
-    alias(libs.plugins.androidLibrary).apply(false)
-    alias(libs.plugins.kotlinMultiplatform).apply(false)
-    alias(libs.plugins.dokka).apply(false)
-    alias(libs.plugins.vanniktech.mavenPublish) apply false
-}
